fb15cf4490dfc9a8f948dbc45b336d2ed2a31884
ngn <ngn13proton@proton.me>
2023-02-23 20:04:01 +0300

    added entry point

--- ipcp/__init__.py
+++ ipcp/__init__.py
@@ -8,20 +8,26 @@ def get_ip(intf):
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
 
-if len(argv)<2:
-    print("Usage: ipcp <interface>")
-    exit()
 
-intf = argv[1]
-intfs = psutil.net_if_addrs()
-
-for i in intfs:
-    if i == intf:
-        ip = get_ip(intf)
-        print("Address: " + intf)
-        pyperclip.copy(ip)
-        print("Copied to clipboard")
+def main():
+    if len(argv)<2:
+        print("Usage: ipcp <interface>")
         exit()
 
-print("Interface not found")
-exit()
+    intf = argv[1]
+    intfs = psutil.net_if_addrs()
+
+    for i in intfs:
+        if i == intf:
+            ip = get_ip(intf)
+            print("Address: " + intf)
+            pyperclip.copy(ip)
+            print("Copied to clipboard")
+            exit()
+
+    print("Interface not found")
+    exit()
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.1" 
+VERSION = "0.0.2" 
 DESCRIPTION = "Simple CLI that lets you copy a specified interface's IP address"
 
 setup(
@@ -10,4 +10,5 @@ setup(
         description=DESCRIPTION,
         packages=find_packages(),
         install_requires=["pyperclip"], 
+        entry_points={'console_scripts': ['ipcp=ipcp.__init__:main']}
 )
